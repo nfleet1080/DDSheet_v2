812c2b5862640197b8a1d943977c7e07c9021eda
nfleet1080 <nfleet1080@gmail.com>
2016-04-08 09:43:08 -0500

    updated to properly reflect new ionic version

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ var path = require('path');
 module.exports = {
   entry: [
     path.normalize('es6-shim/es6-shim.min'),
-    'reflect-metadata',
-    path.normalize('zone.js/dist/zone-microtask'),
-    path.resolve('app/app')
+  'reflect-metadata',
+  path.normalize('zone.js/dist/zone'),
+  path.resolve('app/app')
   ],
   output: {
     path: path.resolve('www/build/js'),
